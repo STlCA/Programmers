3d7cb3f142dbab81f328dd39c23bdb26e46f0609
STIKA <116151737+STlCA@users.noreply.github.com>
2024-01-12 10:13:01 +0900

    [level Lv.0] Title: 숫자 비교하기, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        if ( 0 <= num1 && num1 <= 10000 && 0 <= num2 && num2 <= 10000 )
+        {
+            answer = (num1 == num2) ? 1 : -1 ;
+        }
+        return answer;
+    }
+}
