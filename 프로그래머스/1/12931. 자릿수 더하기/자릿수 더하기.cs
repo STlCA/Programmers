b25c3c940d5c25af47caa80d9b2aa42c90a8bbe4
STIKA <116151737+STlCA@users.noreply.github.com>
2024-01-18 14:43:15 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.45 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -2,15 +2,14 @@ using System;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-while(n > 0)
-{
-    answer += n % 10;
-    n /= 10;
-}        
-        
-return answer;
+            int answer = 0;
+            string str = n.ToString();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                answer += Convert.ToInt32(str[i] - 48);
+            }
+            return answer;  
         
         
     }
